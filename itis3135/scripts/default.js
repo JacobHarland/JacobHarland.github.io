e6c48a9949dada6687eb0769ab1086f1be8cd01b
JacobHarland <geicojacob@gmail.com>
2022-09-13 11:54:41 -0400

    fix titles and file names

--- itis3135/scripts/default.js
+++ itis3135/scripts/default.js
@@ -22,8 +22,7 @@ window.onload = () => {
         .then(data => {
             document.getElementById("footer").innerHTML = data;
         })
-
-
-    document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
-    document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
 }
+
+document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
+document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
