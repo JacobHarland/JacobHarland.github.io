8d956bdd71770963043f54e6c78deabf5e89e0bf
JacobHarland <geicojacob@gmail.com>
2022-09-20 14:23:50 -0400

    move script to bottom test

--- itis3135/scripts/default.js
+++ itis3135/scripts/default.js
@@ -1,2 +1,4 @@
-document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
-document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
+    document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+});
