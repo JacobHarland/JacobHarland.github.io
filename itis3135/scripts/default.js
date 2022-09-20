9381df0b6ddcc103d1e12cac13bee69987f41934
JacobHarland <geicojacob@gmail.com>
2022-09-20 13:33:08 -0400

    Added HTMLInclude

--- itis3135/scripts/default.js
+++ itis3135/scripts/default.js
@@ -1,28 +1,2 @@
-window.onload = () => {
-    fetch('components/nav.html')
-        .then(data => {
-            return data.text()
-        })
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        })
-
-    fetch('components/sidebar.html')
-        .then(data => {
-            return data.text()
-        })
-        .then(data => {
-            document.getElementById("leftSidebar").innerHTML = data;
-        })
-
-    fetch('components/footer.html')
-        .then(data => {
-            return data.text()
-        })
-        .then(data => {
-            document.getElementById("footer").innerHTML = data;
-        })
-}
-
 document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
 document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
