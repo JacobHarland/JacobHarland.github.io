0171ce3fb496fc9a15c50c8fba81130352f9dcef
JacobHarland <geicojacob@gmail.com>
2022-11-03 14:28:07 -0400

    slideshow assignment

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,3 +1,7 @@
+$("p").bind("click", function(){
+    $( "This is a click Event").appendTo( "body" );
+});
+
 function myAlert() {
     alert("This is an alert!");
 }
@@ -41,13 +45,29 @@ function getTodaysDate() {
         document.getElementById('time').innerHTML = time;
     }, 1000);
 }
-// let a;
-// let time;
-// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-// let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-// setInterval(() => {
-//     a = new Date();
-//     time = 'Today Is ' + a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds() + ' on ' + days[a.getDay()] + ', ' + a.getDay() + ' ' + months[a.getMonth()] + ", " + a.getFullYear();
-//     document.getElementById('time').innerHTML = time;
-// }, 1000);
+
+$(document).ready(function() {
+    $('.next').on('click', function() {
+        console.log('clicked')
+        var currentImg = $('.active')
+        var nextImg = currentImg.next();
+
+        if (nextImg.length) {
+            currentImg.removeClass('active').css('z-index', -10)
+            nextImg.addClass('active').css('z-index', 10)
+        }
+    })
+
+    $('.prev').on('click', function() {
+        console.log('clicked')
+        var currentImg = $('.active')
+        var prevImg = currentImg.prev();
+
+        if (prevImg.length) {
+            currentImg.removeClass('active').css('z-index', -10)
+            prevImg.addClass('active').css('z-index', 10)
+        }
+    })
+});
+
 
