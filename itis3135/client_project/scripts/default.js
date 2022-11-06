2259d2c713907421210ade2ab554e1cc505a3f12
JacobHarland <geicojacob@gmail.com>
2022-11-06 18:26:52 -0500

    petsit project

---
+++ itis3135/client_project/scripts/default.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+
+// gets current name
+const activePage = window.location.pathname;
+// loops through each element with data-switcher(nav links) and gets all links associated 
+const navLinks = document.querySelectorAll('[data-switcher]').forEach(link => {
+    // if link includes the name of our active page, it adds the is-active class
+    if (link.href.includes(`${activePage}`)) {
+        link.classList.add('is-active');
+    }
+})
+}
+
+// Gallery on homepage
+$(document).ready(function() {
+    $('.next').on('click', function() {
+        console.log('clicked')
+        var currentImg = $('.active')
+        var nextImg = currentImg.next();
+
+        if (nextImg.length) {
+            currentImg.removeClass('active').css('z-index', -10)
+            nextImg.addClass('active').css('z-index', 10)
+        }
+    })
+
+    $('.prev').on('click', function() {
+        console.log('clicked')
+        var currentImg = $('.active')
+        var prevImg = currentImg.prev();
+
+        if (prevImg.length) {
+            currentImg.removeClass('active').css('z-index', -10)
+            prevImg.addClass('active').css('z-index', 10)
+        }
+    })
+});
+
