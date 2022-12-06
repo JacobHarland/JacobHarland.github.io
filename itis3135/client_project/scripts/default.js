24bd2accbec8c5795a153589a1547264183262e8
JacobHarland <geicojacob@gmail.com>
2022-12-05 20:43:54 -0500

    project stuff

--- itis3135/client_project/scripts/default.js
+++ itis3135/client_project/scripts/default.js
@@ -11,28 +11,47 @@ const navLinks = document.querySelectorAll('[data-switcher]').forEach(link => {
 })
 }
 
-// Gallery on homepage
-$(document).ready(function() {
-    $('.next').on('click', function() {
-        console.log('clicked')
-        var currentImg = $('.active')
-        var nextImg = currentImg.next();
-
-        if (nextImg.length) {
-            currentImg.removeClass('active').css('z-index', -10)
-            nextImg.addClass('active').css('z-index', 10)
-        }
-    })
-
-    $('.prev').on('click', function() {
-        console.log('clicked')
-        var currentImg = $('.active')
-        var prevImg = currentImg.prev();
-
-        if (prevImg.length) {
-            currentImg.removeClass('active').css('z-index', -10)
-            prevImg.addClass('active').css('z-index', 10)
-        }
-    })
+//Gallery on homepage
+$(document).ready(function () {
+    $("#slider").bxSlider({
+        auto: true,
+        randomStart: true,
+        slideWidth: 500,
+        slideMargin: 20,
+        captions: true,
+        speed: 3000,
+        pagerType: 'short',
+        pagerSelector: '#id_pager'
+    });
 });
 
+
+// // Gallery on homepage
+// $(document).ready(function() {
+//     $('.next').on('click', function() {
+//         console.log('clicked')
+//         var currentImg = $('.active')
+//         var nextImg = currentImg.next();
+
+//         if (nextImg.length) {
+//             currentImg.removeClass('active').css('z-index', -10)
+//             nextImg.addClass('active').css('z-index', 10)
+//         }
+//     })
+
+//     $('.prev').on('click', function() {
+//         console.log('clicked')
+//         var currentImg = $('.active')
+//         var prevImg = currentImg.prev();
+
+//         if (prevImg.length) {
+//             currentImg.removeClass('active').css('z-index', -10)
+//             prevImg.addClass('active').css('z-index', 10)
+//         }
+//     })
+// });
+
+// FAQ page accordion
+$(function () {
+    $("#accordion").accordion();
+});
